17eb23219a2198c3de73bcd5ae25820a278b075e
igwe felix <felixigwe55@gmail.com>
2022-10-18 15:10:18 -0700

    checked

--- printf_int.c
+++ printf_int.c
@@ -1,11 +1,9 @@
 #include "main.h"
-
 /**
  * printf_int - prints integer
  * @args: argument to print
  * Return: number of characters printed
  */
-
 int printf_int(va_list args)
 {
 	int n = va_arg(args, int);
